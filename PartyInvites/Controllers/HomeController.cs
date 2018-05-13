c54496aa6c906da53894630d9f206a6e15ff24fe
naderscode <naderscode@gmail.com>
2018-05-13 16:51:02 -0700

    Initial Commit

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ViewResult Index() {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView");
+        }
+/*
+        public string Index() {
+            return "Hello World";
+        }
+        
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        */
+    }
+}
